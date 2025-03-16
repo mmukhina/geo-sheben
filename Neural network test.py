69c3bcb4d4da5bcea7574f03060aeb6455f463d4
KeySi <skvclass@yandex.ru>
2025-03-16 21:33:45 +0300

    В одном файле код по созданию и обучению нейронки. Во втором - подгрузка модели и тестирование

---
+++ Neural network test.py
@@ -0,0 +1,95 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import PIL
+import tensorflow as tf
+import pathlib
+import os
+
+from tensorflow import keras
+from tensorflow.keras import layers
+from tensorflow.keras.models import Sequential
+from tensorflow.keras import regularizers
+
+dataset_dir = pathlib.Path("flower_photos/")
+image_count = len(list(dataset_dir.glob("*/*.jpg")))
+print(f"Всего изображений: {image_count}")
+
+batch_size = 32
+img_width = 180
+img_height = 180
+
+train_ds = tf.keras.utils.image_dataset_from_directory(
+	dataset_dir,
+	validation_split=0.2,
+	subset="training",
+	seed=123,
+	image_size=(img_height, img_width),
+	batch_size=batch_size)
+
+val_ds = tf.keras.utils.image_dataset_from_directory(
+	dataset_dir,
+	validation_split=0.2,
+	subset="validation",
+	seed=123,
+	image_size=(img_height, img_width),
+	batch_size=batch_size)
+
+class_names = train_ds.class_names
+print(f"Class names: {class_names}")
+
+num_classes = len(class_names)
+model = Sequential([
+	layers.Rescaling(1./255, input_shape=(img_height, img_width, 3)),
+
+	# аугментация
+	layers.RandomFlip("horizontal", input_shape=(img_height, img_width, 3)),
+	layers.RandomRotation(0.1),
+	layers.RandomZoom(0.1),
+	layers.RandomContrast(0.2),
+
+	layers.Conv2D(8, 3, padding='same', activation='relu'),
+	layers.MaxPooling2D(),
+
+	layers.Conv2D(16, 3, padding='same', activation='relu'),
+	layers.MaxPooling2D(),
+
+	layers.Conv2D(32, 3, padding='same', activation='relu'),
+	layers.MaxPooling2D(),
+
+	layers.Conv2D(64, 3, padding='same', activation='relu'),
+	layers.MaxPooling2D(),
+
+	# регуляризация
+	layers.Dropout(0.2),
+
+	layers.Flatten(),
+	layers.Dense(128, activation='relu'),
+	layers.Dense(64, kernel_regularizer=regularizers.l2(0.01)),
+	layers.Dense(32, activation='relu'),
+	layers.Dense(num_classes)
+])
+
+model.compile(
+    optimizer='adam',
+    loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+    metrics=['accuracy'])
+
+# Evaluate the model
+loss, acc = model.evaluate(val_ds, verbose=2)
+print("Untrained model, accuracy: {:5.2f}%".format(100 * acc))
+
+checkpoint_path = "training_1/cp.weights.h5"
+model.load_weights(checkpoint_path)
+
+# Re-evaluate the model
+loss, acc = model.evaluate(val_ds, verbose=2)
+print("Restored model, accuracy: {:5.2f}%".format(100 * acc))
+
+image = tf.keras.utils.load_img("9.jpg", target_size=(img_height, img_width))
+img_array = tf.keras.utils.img_to_array(image)
+img_array = tf.expand_dims(img_array, 0)
+
+predictions = model.predict(img_array)
+score = tf.nn.softmax(predictions)
+
+print(f"На изображении {class_names[np.argmax(score)]} ({100 * np.max(score)})")
---
+++ Neural network.py
@@ -0,0 +1,132 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import PIL
+import tensorflow as tf
+import pathlib
+import os
+
+from tensorflow import keras
+from tensorflow.keras import layers
+from tensorflow.keras.models import Sequential
+from tensorflow.keras import regularizers
+from tensorflow.keras.callbacks import EarlyStopping
+
+
+dataset_dir = pathlib.Path("flower_photos/")
+image_count = len(list(dataset_dir.glob("*/*.jpg")))
+print(f"Всего изображений: {image_count}")
+
+batch_size = 32
+img_width = 180
+img_height = 180
+
+train_ds = tf.keras.utils.image_dataset_from_directory(
+	dataset_dir,
+	validation_split=0.2,
+	subset="training",
+	seed=123,
+	image_size=(img_height, img_width),
+	batch_size=batch_size)
+
+val_ds = tf.keras.utils.image_dataset_from_directory(
+	dataset_dir,
+	validation_split=0.2,
+	subset="validation",
+	seed=123,
+	image_size=(img_height, img_width),
+	batch_size=batch_size)
+
+class_names = train_ds.class_names
+print(f"Class names: {class_names}")
+
+# cache
+# AUTOTUNE = tf.data.AUTOTUNE
+# train_ds = train_ds.cache().shuffle(1000).prefetch(buffer_size=AUTOTUNE)
+# val_ds = val_ds.cache().prefetch(buffer_size=AUTOTUNE)
+
+# create model
+num_classes = len(class_names)
+model = Sequential([
+	layers.Rescaling(1./255, input_shape=(img_height, img_width, 3)),
+
+	# аугментация
+	layers.RandomFlip("horizontal", input_shape=(img_height, img_width, 3)),
+	layers.RandomRotation(0.1),
+	layers.RandomZoom(0.1),
+	layers.RandomContrast(0.2),
+
+	layers.Conv2D(8, 3, padding='same', activation='relu'),
+	layers.MaxPooling2D(),
+
+	layers.Conv2D(16, 3, padding='same', activation='relu'),
+	layers.MaxPooling2D(),
+
+	layers.Conv2D(32, 3, padding='same', activation='relu'),
+	layers.MaxPooling2D(),
+
+	layers.Conv2D(64, 3, padding='same', activation='relu'),
+	layers.MaxPooling2D(),
+
+	# регуляризация
+	layers.Dropout(0.2),
+
+	layers.Flatten(),
+	layers.Dense(128, activation='relu'),
+	layers.Dense(64, kernel_regularizer=regularizers.l2(0.01)),
+	layers.Dense(32, activation='relu'),
+	layers.Dense(num_classes)
+])
+
+# compile the model
+model.compile(
+	optimizer='adam',
+	loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+	metrics=['accuracy'])
+
+# print model summary
+model.summary()
+
+checkpoint_path = "training_1/cp.weights.h5"
+checkpoint_dir = os.path.dirname(checkpoint_path)
+
+# Create a callback that saves the model's weights
+cp_callback = tf.keras.callbacks.ModelCheckpoint(
+    filepath=checkpoint_path,
+    verbose=1,
+    save_weights_only=True,
+    save_freq=batch_size)
+
+early_stopping = EarlyStopping(monitor='val_loss', patience=3)
+
+epochs = 20 # количество эпох тренировки
+history = model.fit(
+	train_ds,
+	validation_data=val_ds,
+	epochs=epochs,
+	callbacks=[cp_callback, early_stopping])
+
+# visualize training and validation results
+acc = history.history['accuracy']
+val_acc = history.history['val_accuracy']
+
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+
+epochs_range = range(epochs)
+
+plt.figure(figsize=(8, 8))
+plt.subplot(1, 2, 1)
+plt.plot(epochs_range, acc, label='Training Accuracy')
+plt.plot(epochs_range, val_acc, label='Validation Accuracy')
+plt.legend(loc='lower right')
+plt.title('Training and Validation Accuracy')
+
+plt.subplot(1, 2, 2)
+plt.plot(epochs_range, loss, label='Training Loss')
+plt.plot(epochs_range, val_loss, label='Validation Loss')
+plt.legend(loc='upper right')
+plt.title('Training and Validation Loss')
+plt.show()
+
+model.save_weights('flower_model.weights.h5')
+print("Model_saved")
